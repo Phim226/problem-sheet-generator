340d2a08c568b46c91ce36055549bc01dfa4ec60
Andrew Phimister <andyph1@hotmail.com>
2025-10-26 03:08:47 +0000

    Wrote curve generation function

--- mathematics/geometry.py
+++ mathematics/geometry.py
@@ -1,4 +1,5 @@
-from sympy import Symbol, latex
+from random import choices
+from sympy import Expr, Symbol, latex
 from sympy.abc import t, theta
 from sympy.vector import ParametricRegion
 
@@ -14,9 +15,17 @@ class Curve():
     J_HAT_LATEX = r"\mathbf{{\hat{{j}}}}"
     K_HAT_LATEX = r"\mathbf{{\hat{{k}}}}"
 
-    def __init__(self, p: Symbol):
+    def __init__(self, p: Symbol, dimension: int):
+
+        if dimension not in (2, 3):
+            raise ValueError((f"{dimension} is not a valid dimension. "
+                            "It should be 2 or 3.")
+                    )
+        self._dimension = dimension
+
         self._curve = ParametricRegion((p, p, 2*p**2), (p, 0, 1))
         self._curve_latex = self._format_curve_latex(self._curve)
+
         if p is t:
             print(f"Symbol is {t}")
         elif p is theta:
@@ -29,6 +38,60 @@ class Curve():
     @property
     def curve(self):
         return self._curve
+
+    @staticmethod
+    def _generate_random_polynomial(p: Symbol) -> Expr:
+        coeffs = [0, 0, 0, 0]
+
+        min_num_non_zero = 1
+        max_num_non_zero = 2
+        number_of_coeffs: int = choices(
+            population = range(
+                min_num_non_zero,
+                max_num_non_zero + 1
+            ),
+            weights = [0.6, 0.4]
+        )[0]
+
+        smallest_coeff_value = -4
+        highest_coeff_value = 4
+        coeff_range: list[int] = list(range(
+            smallest_coeff_value,
+            highest_coeff_value + 1
+            )
+        )
+        coeff_range.remove(0)
+
+        index_range: list[int] = list(range(len(coeffs)))
+
+        for _ in range(number_of_coeffs):
+            index: int = choices(population = index_range)[0]
+            index_range.remove(index)
+            coeffs[index] = choices(
+                population = coeff_range,
+                weights = [0.01, 0.05, 0.05, 0.1, 0.4, 0.2, 0.1, 0.09]
+            )[0]
+
+        return coeffs[0]*p**3 + coeffs[1]*p**2 + coeffs[2]*p + coeffs[3]
+
+    def _generate_random_parametric_curve(
+            self,
+            p: Symbol,
+            dimension: int
+    ) -> ParametricRegion:
+
+        x_component: Expr = self._generate_random_polynomial(p)
+        y_component: Expr = self._generate_random_polynomial(p)
+        z_component: Expr = (
+            self._generate_random_polynomial(p) if dimension == 3 else 0
+            )
+        return ParametricRegion((
+            x_component,
+            y_component,
+            z_component),
+            (p, 0, 1)
+        )
+
 
     def _format_curve_latex(self, curve: ParametricRegion) -> str:
         curve_def_x: str = latex(curve.definition[0])
