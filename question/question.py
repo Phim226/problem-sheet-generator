4beff1678773e1eaa5755d858399ca05fb08b9b5
Andrew Phimister <andyph1@hotmail.com>
2025-10-30 22:05:24 +0000

    Included some logic regarding subtopics

--- question/question.py
+++ question/question.py
@@ -1,12 +1,10 @@
 import logging
-from typing import Any
 from abc import ABC, abstractmethod
-from sympy import Rational
+from sympy import Rational, latex
 from sympy.abc import t
-from sympy.vector import Vector
 from pylatex.utils import NoEscape
 from question.question_registry import register_question_type
-from mathematics.vector_calculus import VectorField
+from mathematics.vector_calculus import ScalarField, VectorField
 from mathematics.geometry import Curve
 from utilities.mathematics import clumsy_rational
 
@@ -21,16 +19,26 @@ class Question(ABC):
         self._difficulty: str = difficulty
 
     @property
-    def topic(self):
+    def topic(self) -> str:
         return self._topic
 
     @property
-    def answer(self):
+    def answer(self) -> str:
         return self._answer
 
     @property
-    def question_latex(self):
-        return self._question_latex
+    def question(self) -> str:
+        return self._question
+
+    @abstractmethod
+    def _generate_question_latex(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def _generate_answer_latex(self, *args, **kwargs):
+        pass
+
+
 
 @register_question_type("vector_calculus")
 class VectorCalculusQuestion(Question):
@@ -41,20 +49,36 @@ class VectorCalculusQuestion(Question):
     # TODO: Improve question LaTeX generation logic based on question subtopic etc.
     # TODO: Properly format answer LaTeX.
     def __init__(
-            self, topic: str, subtopic: str = "",
-            dimension: int = 3, curve_is_parametric: bool = True,
-            curve_is_implicit: bool = False, **kwargs: bool):
+            self, topic: str,
+            subtopic: str = "",
+            dimension: int = 3,
+            curve_is_parametric: bool = True,
+            curve_is_implicit: bool = False,
+            **kwargs: bool
+    ):
         super().__init__(topic)
         self._dimension = dimension
 
-        answer_is_clumsy = True
-        while answer_is_clumsy:
-            curve: Curve = Curve(t, dimension)
-            vector_field: VectorField = VectorField(dimension)
-            answer: Rational = vector_field.calculate_line_integral(curve.curve)
-            answer_is_clumsy = clumsy_rational(answer)
-        self._answer: Any = answer
-        self._question_latex: str = self._generate_question_latex(vector_field, curve)
+        if topic == "line_integral":
+            if subtopic not in ("vector_field",
+                                "scalar_field",
+                                "fundamental_thm_line_integrals"):
+                msg = (f"{subtopic} is not a valid subtopic for the "
+                       "line_integral topic")
+                raise ValueError(msg)
+
+            answer_is_clumsy = True
+            while answer_is_clumsy:
+                curve: Curve = Curve(t, dimension)
+                field: VectorField | ScalarField = (
+                    VectorField(dimension) if subtopic == "vector_field"
+                    else ScalarField(dimension)
+                )
+
+                answer: Rational = field.calculate_line_integral(curve.curve)
+                answer_is_clumsy = clumsy_rational(answer)
+        self._answer: str = self._generate_answer_latex(answer)
+        self._question: str = self._generate_question_latex(field, curve)
 
     def _generate_question_latex(self, vector_field: VectorField, curve: Curve) -> str:
         return NoEscape(
@@ -64,3 +88,7 @@ class VectorCalculusQuestion(Question):
             r"Calculate $\displaystyle\int_C"
             rf"{self.VECTOR_FIELD_SYMBOL_LATEX}\cdot\mathbf{{dr}}$."
         )
+
+    @staticmethod
+    def _generate_answer_latex(answer: str) -> str:
+        return NoEscape(f"${latex(answer)}$")
