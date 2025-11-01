763be395c39564f1cc5919d4c5467d3591b46431
Andrew Phimister <andyph1@hotmail.com>
2025-11-01 15:23:01 +0000

    Refactored code for cleanliness

--- mathematics/vector_calculus.py
+++ mathematics/vector_calculus.py
@@ -1,10 +1,12 @@
-from logging import info
 from abc import abstractmethod
+from functools import reduce
+from logging import info
 from random import random
 from sympy import (Expr,
                    S,
                    factor_terms)
-from sympy.vector import (CoordSys3D, ParametricRegion, Vector,
+from sympy.vector import (BaseScalar, CoordSys3D,
+                          ParametricRegion, Vector, VectorZero,
                           vector_integrate)
 from utilities.latex_formatting import CleanVectorLatexPrinter
 from utilities.mathematics import (polynomial_from_coeffs,
@@ -30,11 +32,15 @@ class Field():
         return self._name
 
     @property
+    def name_latex(self) -> str:
+        return self._name_latex
+
+    @property
     def dimension(self) -> int:
         return self._dimension
 
     @property
-    def field(self) -> Expr | Vector:
+    def field(self) -> Expr:
         return self._field
 
     @property
@@ -43,15 +49,14 @@ class Field():
 
     # TODO: Change weights depending on whether field is vector or scalar
     def _generate_random_component(self, allow_zero: bool = True) -> Expr:
-        if allow_zero:
-            return_zero: bool = random() < 0.05
-            if return_zero:
-                return S.Zero
+        if allow_zero and random() < 0.05:
+            return S.Zero
 
         # These index weights will favour smaller degree expressions
         index_weights = [0.5, 1, 1, 0.5, 1, 1]
         if self._dimension == 3:
             index_weights += [0.5, 1, 1] # The weights for the z coefficient indices
+
         coeffs: list[int] = random_weighted_coefficients(
             max_index = 3*self._dimension,
             non_zero_coeffs_range = (1, 4),
@@ -60,24 +65,21 @@ class Field():
             coeff_value_weights = [0.01, 0.05, 0.05, 0.1, 0.4, 0.2, 0.1, 0.09],
             index_weights = index_weights
         )
-        x_coeffs: list[int] = coeffs[0:3]
-        y_coeffs: list[int] = coeffs[3:6]
-        z_coeffs: list[int] = coeffs[6:9] if self._dimension == 3 else None
+        x_coeffs, y_coeffs, z_coeffs = (coeffs[0:3], coeffs[3:6],
+                                        coeffs[6:9] if self._dimension == 3 else None)
 
-        component_x_terms: Expr = (
-            1 if all(c == 0 for c in x_coeffs)
-            else polynomial_from_coeffs(self._C.x, x_coeffs)
-        )
-        component_y_terms: Expr = (
-            1 if all(c == 0 for c in y_coeffs)
-            else polynomial_from_coeffs(self._C.y, y_coeffs)
-        )
-        component_z_terms: Expr = (
-            1 if not z_coeffs or all(c == 0 for c in z_coeffs)
-            else polynomial_from_coeffs(self._C.z, z_coeffs)
-        )
+        def make_component_terms(scalar: BaseScalar, coeffs: list[int]) -> Expr:
+            return (S.One if not coeffs or all(c == 0 for c in coeffs)
+                    else polynomial_from_coeffs(scalar, coeffs))
 
-        return component_x_terms*component_y_terms*component_z_terms
+        terms = [
+            make_component_terms(self._C.x, x_coeffs),
+            make_component_terms(self._C.y, y_coeffs)
+        ]
+        if self.dimension == 3:
+            terms.append(make_component_terms(self._C.z, z_coeffs))
+
+        return reduce(lambda a, b: a*b, terms, S.One)
 
 class ScalarField(Field):
 
@@ -91,40 +93,36 @@ class ScalarField(Field):
 class VectorField(Field):
 
 
-    VECTOR_FIELD_SYMBOL_LATEX = r"\mathbf{{F}}"
-
     def __init__(self, name: str, dimension: int):
         super().__init__(name, dimension)
 
-        all_components_zero = True
-        while all_components_zero:
-            self._x_component: Expr = factor_terms(
-                self._generate_random_component(),
-                sign = True
-            )
-            self._y_component: Expr = factor_terms(
-                self._generate_random_component(),
-                sign = True
-            )
-            self._z_component: Expr = factor_terms(
-                self._generate_random_component(),
-                sign = True
-            ) if dimension == 3 else S.Zero
+        self._name_latex = rf"\mathbf{{{name}}}"
 
-            all_components_zero = (
-                    (self._x_component is S.Zero) and
-                    (self._y_component is S.Zero) and
-                    (self._z_component is S.Zero)
-            )
+        while True:
+            self._components: list[Expr] = [
+                factor_terms(
+                    self._generate_random_component(),
+                    sign = True
+                ) for _ in range(dimension)
+            ]
+            if not all(c is S.Zero for c in self._components):
+                break
 
-        self._field: Vector = (self._x_component*self._C.i +
-                               self._y_component*self._C.j +
-                               self._z_component*self._C.k)
-        info(f"Vector field expression: {self.field}")
+        field_dict = {
+            vect: comp
+            for vect, comp in zip(self._C.base_vectors(), self._components)
+        }
+
+        self._field: Vector = sum(
+            (comp*vect for vect, comp in field_dict.items()),
+            VectorZero()
+        )
+
+        info(f"Vector field expression: {self._field}")
 
         printer: CleanVectorLatexPrinter = CleanVectorLatexPrinter()
         self._field_latex: str = printer.vector_field_print(self)
 
 
     def calculate_line_integral(self, curve: ParametricRegion):
-        return vector_integrate(self.field, curve)
+        return vector_integrate(self._field, curve)
