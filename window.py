3139cb683e60b551395f82d829d32a6e11bd461b
Andrew Phimister <andyph1@hotmail.com>
2025-11-01 01:21:55 +0000

    Experimenting with TreeView

--- window.py
+++ window.py
@@ -1,5 +1,6 @@
 from logging import info
-from tkinter import Button, Tk
+from tkinter import Button, Canvas, Tk
+from tkinter.ttk import Treeview
 from main import create_document
 from question.question_registry import KEYWORD_REGISTRY, QUESTION_REGISTRY
 from utilities.misc import configure_log
@@ -11,7 +12,7 @@ from utilities.misc import configure_log
 # TODO: Temporarily store files and give option to save or export them for permanant storage.
 # TODO: Have problem sheet templates with typical collections of questions.
 # TODO: Each problem sheet stored in a ProblemSheet object containing #questions, question topics etc.
-# TODO: Have left most column full list of questions, column to the right questions in current ProblemSheet, with "add" and "remove" buttons.
+# TODO: Have left most column full list of questions, column to the right questions is current ProblemSheet, with "add" and "remove" buttons.
 if __name__ == "__main__":
     configure_log()
 
@@ -21,11 +22,24 @@ if __name__ == "__main__":
     root.title("Problem Sheet Generator")
     root.geometry("500x500")
 
-    button = Button(
+    """ button = Button(
         root,
         text = "Generate Problem Sheet",
         command = create_document
     )
-    button.grid()
+    button.grid() """
 
+    tree = Treeview(root)
+    # Inserted at the root, program chooses id:
+    tree.insert('', 'end', 'widgets', text='Widget Tour')
+
+    # Same thing, but inserted as first child:
+    tree.insert('', 0, 'gallery', text='Applications')
+
+    # Treeview chooses the id:
+    id = tree.insert('', 'end', text='Tutorial')
+
+    # Inserted underneath an existing node:
+    tree.insert('widgets', 'end', text='Canvas')
+    tree.insert(id, 'end', text='Tree')
     root.mainloop()
