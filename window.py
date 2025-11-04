b5b65e7d5e0b17fdecff0e5dc23fe7f75b9d292b
Andrew Phimister <andyph1@hotmail.com>
2025-11-04 01:25:01 +0000

    Improving treeview mockup, still non-functional

--- window.py
+++ window.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
 
     info(f"Question registry: {QUESTION_REGISTRY}")
     info(f"Keyword registry: {KEYWORD_REGISTRY}")
+
     root = Tk()
     root.title("Problem Sheet Generator")
     root.geometry("500x500")
@@ -29,17 +30,16 @@ if __name__ == "__main__":
     )
     button.grid() """
 
-    tree = Treeview(root)
-    # Inserted at the root, program chooses id:
-    tree.insert('', 'end', 'widgets', text='Widget Tour')
+    question_tree = Treeview(root, show = "tree headings")
+    question_tree.heading("#0", text = "Questions")
+    question_tree.insert("", "end", "vect_calc", text = "Vector Calculus")
+    question_tree.insert("vect_calc", "end", "line_integral", text = "Line integrals")
+    question_tree.insert("line_integral", "end", "vector_field", text = "Vector fields")
+    question_tree.insert("line_integral", "end", "scalar_field", text = "Scalar fields")
+    question_tree.grid(row = 0, column = 0)
 
-    # Same thing, but inserted as first child:
-    tree.insert('', 0, 'gallery', text='Applications')
+    selected_tree = Treeview(root, show = "tree headings")
+    selected_tree.heading("#0", text = "Selected questions")
+    selected_tree.grid(row = 0, column = 1)
 
-    # Treeview chooses the id:
-    id = tree.insert('', 'end', text='Tutorial')
-
-    # Inserted underneath an existing node:
-    tree.insert('widgets', 'end', text='Canvas')
-    tree.insert(id, 'end', text='Tree')
     root.mainloop()
