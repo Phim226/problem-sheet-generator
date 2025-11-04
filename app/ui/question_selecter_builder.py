154cd1719ad46955ab4f9aedf5c4d554965c8a79
Andrew Phimister <andyph1@hotmail.com>
2025-11-04 16:16:35 +0000

    Question selection list now built from registries

--- app/ui/question_selecter_builder.py
+++ app/ui/question_selecter_builder.py
@@ -1,10 +1,13 @@
 from tkinter import Tk
 from tkinter.ttk import Treeview
+from question.question import Question
+from question.question_registry import TOPIC_REGISTRY, QUESTION_REGISTRY
 
 # TODO: Have left most column full list of questions, column to the right questions is current ProblemSheet, with "add" and "remove" buttons.
 # TODO: Double click moves question to selected questions.
 # TODO: Questions list have tick boxes, pressing "add" adds all selected questions.
 # TODO: Have method of adding topics and subtopics to questions list.
+# TODO: Give users ability to edit questions in question selecter
 class QuestionSelecterBuilder():
 
 
@@ -15,18 +18,29 @@ class QuestionSelecterBuilder():
         self._build_question_tree()
         self._selected_question_tree()
 
+
+
     def _build_question_tree(self):
-        question_tree = Treeview(self._root, show = "tree headings")
+        question_tree: Treeview = Treeview(self._root, show = "tree headings")
         question_tree.heading("#0", text = "Questions")
-        question_tree.insert("", "end", "vect_calc", text = "Vector Calculus")
-        question_tree.insert("vect_calc", "end", "line_integral", text = "Line integrals")
-        question_tree.insert("line_integral", "end", "vector_field", text = "Vector fields")
-        question_tree.insert("line_integral", "end", "scalar_field", text = "Scalar fields")
+
+        question_types: list[Question] = sorted(list(TOPIC_REGISTRY.keys())[1:])
+        for name in question_types:
+            question_tree.insert("", "end", name, text = name)
+
+            topics = QUESTION_REGISTRY[name].subtopics.keys()
+            for topic in topics:
+                question_tree.insert(name, "end", topic, text = topic)
+
+                subtopics = TOPIC_REGISTRY[name][topic]
+                for subtopic in subtopics:
+                    question_tree.insert(topic, "end", f"{topic}_{subtopic}", text = subtopic)
+
         question_tree.grid(row = 0, column = 0)
         self._question_tree: Treeview = question_tree
 
     def _selected_question_tree(self):
         selected_tree = Treeview(self._root, show = "tree headings")
-        selected_tree.heading("#0", text = "Selected questions")
+        selected_tree.heading("#0", text = "Selected Questions")
         selected_tree.grid(row = 0, column = 1)
         self._selected_tree: Treeview = selected_tree
