d04ddec3899a25603b596370ad774ae81969ef24
Andrew Phimister <andyph1@hotmail.com>
2025-11-05 14:20:02 +0000

    Testing double mouse click event

--- app/ui/question_selecter_builder.py
+++ app/ui/question_selecter_builder.py
@@ -1,5 +1,5 @@
-from tkinter import Tk
-from tkinter.ttk import Treeview
+from tkinter import Event, Tk
+from ttkbootstrap import Button, Frame, Scrollbar, Treeview
 from question.question import Question
 from question.question_registry import TOPIC_REGISTRY, QUESTION_REGISTRY
 
@@ -13,34 +13,62 @@ class QuestionSelecterBuilder():
 
     def __init__(self, root: Tk):
         self._root = root
+        self._selecter_frame = Frame(root)
+        self._selecter_frame.pack(side = "top", anchor = "nw")
 
-    def build(self):
-        self._build_question_tree()
-        self._selected_question_tree()
+    @property
+    def question_tree(self) -> Treeview:
+        return self._question_tree
 
+    @property
+    def selection_tree(self) -> Treeview:
+        return self._selected_tree
 
+    def build(self) -> None:
+        self._question_tree: Treeview = self._build_question_tree("Questions")
+        self._populate_question_tree(self._question_tree)
 
-    def _build_question_tree(self):
-        question_tree: Treeview = Treeview(self._root, show = "tree headings")
-        question_tree.heading("#0", text = "Questions")
+        self._selected_tree: Treeview = self._build_question_tree("Selected Questions")
 
+    @staticmethod
+    def _populate_question_tree(tree: Treeview) -> None:
         question_types: list[Question] = sorted(list(TOPIC_REGISTRY.keys())[1:])
         for name in question_types:
-            question_tree.insert("", "end", name, text = name)
+            tree.insert("", "end", name, text = name)
 
             topics = QUESTION_REGISTRY[name].subtopics.keys()
             for topic in topics:
-                question_tree.insert(name, "end", topic, text = topic)
+                tree.insert(name, "end", topic, text = topic)
 
                 subtopics = TOPIC_REGISTRY[name][topic]
                 for subtopic in subtopics:
-                    question_tree.insert(topic, "end", f"{topic}_{subtopic}", text = subtopic)
+                    tree.insert(topic, "end", f"{topic}_{subtopic}", text = subtopic)
 
-        question_tree.grid(row = 0, column = 0)
-        self._question_tree: Treeview = question_tree
 
-    def _selected_question_tree(self):
-        selected_tree = Treeview(self._root, show = "tree headings")
-        selected_tree.heading("#0", text = "Selected Questions")
-        selected_tree.grid(row = 0, column = 1)
-        self._selected_tree: Treeview = selected_tree
+    def _build_question_tree(self, title: str) -> Treeview:
+        tree_frame: Frame = Frame(self._selecter_frame)
+        tree_frame.pack(side = "left")
+
+        button = Button(tree_frame,
+                        text = "add" if title == "Questions" else "remove")
+        button.pack(side = "bottom", anchor = "se")
+
+        tree = Treeview(tree_frame, show = "tree headings")
+        tree.heading("#0", text = title)
+        tree.pack(side = "left")
+
+        vertical_scroll = Scrollbar(tree_frame, orient = "vertical", command = tree.yview)
+        vertical_scroll.pack(side = "right", fill = "y")
+
+        tree.configure(yscrollcommand = vertical_scroll.set)
+
+        tree.bind("<Double-1>", self._double_click)
+
+        tree.widget_id = f"{title.replace(" ", "_").lower()}_tree"
+
+        return tree
+
+    def _double_click(self, event: Event):
+        print(event.widget.widget_id)
+        widget: Treeview = event.widget
+        print(widget.identify_row(event.y))
