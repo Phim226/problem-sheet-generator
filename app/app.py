0ce097478b5670c8e361dbcae6e0302655215f2e
Andrew Phimister <andyph1@hotmail.com>
2025-11-05 14:17:58 +0000

    Added ttkbootstrap style

--- app/app.py
+++ app/app.py
@@ -1,5 +1,8 @@
 from tkinter import Tk
+from ttkbootstrap import Style
 from app.ui.question_selecter_builder import QuestionSelecterBuilder
+
+THEME: str = "darkly"
 
 class ProblemSheetGeneratorApp():
 
@@ -10,4 +13,15 @@ class ProblemSheetGeneratorApp():
         self._root = root
 
     def build(self):
+        self._configure_style()
         QuestionSelecterBuilder(self._root).build()
+
+    @staticmethod
+    def _configure_style():
+        style = Style(theme = THEME)
+        style.configure(
+        "Treeview.Heading",
+        background="#36434E",
+        foreground="#CFCFCF",
+        font=("Segoe UI", 9, "bold")
+    )
