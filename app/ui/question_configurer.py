7d75683ae7be1a9bc58049194a25e7ec920b53fc
Andrew Phimister <andyph1@hotmail.com>
2025-11-08 00:23:41 +0000

    Configurer has entries and generate button

--- app/ui/question_configurer.py
+++ app/ui/question_configurer.py
@@ -1,5 +1,6 @@
 from tkinter import Tk
-from ttkbootstrap import Button, Frame, Label, Labelframe
+from ttkbootstrap import Button, Checkbutton, Entry, Frame, IntVar, Label, Labelframe, StringVar
+from main import create_document
 
 class QuestionConfigurer():
 
@@ -9,6 +10,45 @@ class QuestionConfigurer():
         self._config_frame = Labelframe(root, text = "Configuration")
         self._config_frame.pack(side = "top", anchor = "nw")
 
+    @property
+    def problem_name_entry(self):
+        return self._problem_name_entry
+
+    @property
+    def answer_name_entry(self):
+        return self._answer_name_entry
+
+    @property
+    def tex_check(self):
+        return self._tex_check
+
 
     def build(self):
-        Label(self._config_frame, text = "Questions").grid(row = 0, column = 0)
+        Label(self._config_frame, text = "Problem sheet name: ").grid(row = 0, column = 0)
+        self._problem_name_entry: Entry = Entry(self._config_frame)
+        self._problem_name_entry.insert(0, "Problem_Sheet")
+        self._problem_name_entry.grid(row = 0, column = 1, padx = 2, pady = 2)
+
+        Label(self._config_frame, text = "Answer sheet name: ").grid(row = 1, column = 0)
+        self._answer_name_entry: Entry = Entry(self._config_frame, text = "Answer_Sheet")
+        self._answer_name_entry.insert(0, "Answer_Sheet")
+        self._answer_name_entry.grid(row = 1, column = 1, padx = 2, pady = 2)
+
+        self._tex_var = IntVar(value = 1)
+        self._tex_check = Checkbutton(
+            self._config_frame,
+            text = "Generate tex file",
+            variable = self._tex_var,
+            onvalue = 1,
+            offvalue = 0
+        )
+        self._tex_check.grid(row = 2, column = 0, pady = 2)
+
+        button = Button(
+            self._config_frame,
+            text = "Generate Problem Sheet",
+            command = create_document
+        )
+        button.grid(row = 3, column = 1, padx = 4, pady = 4)
+
+
