6960d6aa789df1332702b3e91b68f26c4f3154bc
Andrew Phimister <andyph1@hotmail.com>
2025-11-19 04:37:20 +0000

    Introduced Regenerating class for objs that regen

---
+++ core/regenerating.py
@@ -0,0 +1,7 @@
+class Regenerating():
+
+    def regenerate(self):
+        self._regenerate()
+
+    def _regenerate(self):
+        raise NotImplementedError("Subclasses must implement _regenerate()")
