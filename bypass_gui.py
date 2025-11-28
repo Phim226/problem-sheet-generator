fc986f2da5e3dba64ae02790bfeb024c32e5ae04
Andrew Phimister <andyph1@hotmail.com>
2025-11-28 22:57:30 +0000

    Wrote TODO

--- bypass_gui.py
+++ bypass_gui.py
@@ -1,10 +1,12 @@
 from logging import info
-from app.ui.question_configurer import SheetConfig
+from app.ui.sheet_creation.question_configurator import SheetConfig
+from app.ui.sheet_creation.question_selector import QuestionConfig
 from core.sheet_generator import SheetGenerator
 from core.question.question_registry import KEYWORD_REGISTRY, QUESTION_REGISTRY
 from utilities.misc import configure_log
 
 # Use this code to bypass GUI.
+# TODO: Need to manually create QuestionConfig and pass appropriate arguments to generate() before this code can be used.
 if __name__ == "__main__":
     configure_log()
 
