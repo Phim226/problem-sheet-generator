9f4aa4133f041bd37e971dbd7f76cf04ce2d20b7
Andrew Phimister <andyph1@hotmail.com>
2025-11-29 17:40:19 +0000

    Question subclasses created

--- core/question/multivariable_calculus_question.py
+++ core/question/multivariable_calculus_question.py
@@ -6,52 +6,66 @@ from core.mathematics.multivariable_calculus import ScalarField, VectorField
 from core.mathematics.geometry import Curve
 from utilities.mathematics import awkward_number
 
-class MultivariableCalculusQuestion(Question):
+@register_type()
+class MultivariableCalculusQuestion(Question, ABC):
+
+    name: tuple[str] = ("multivariable_calc", "Multivariable calculus")
+
+    def __init__(
+            self, topic: str,
+            subtopic: str,
+            **kwargs: bool
+    ):
+        super().__init__(topic, subtopic, **kwargs)
 
 
-    subtopics: dict[str, list[str]] = {
-        "line_integrals": ["scalar_field", "vector_field", "fundamental_theorem_of_line_integrals",
-                          "conservative_vector_field", "green's_theorem"],
-        "surface_integrals": ["scalar_field", "vector_field", "stoke's_theorem", "divergence_theorem"]
-    }
+@register_question()
+class LineIntegralQuestion(MultivariableCalculusQuestion):
+
+    topic: tuple[str] = ("line_integral", "Line integral")
 
     # TODO: Improve question LaTeX generation logic based on question subtopic etc.
     # TODO: Properly format answer LaTeX.
-    def __init__(
-            self, topic: str,
-            subtopic: str = "",
-            dimension: int = 3,
-            curve_is_parametric: bool = True,
-            curve_is_implicit: bool = False,
-            **kwargs: bool
-    ):
-        super().__init__(topic)
+
+    """ subtopics: dict[str, str] = {
+        "scalar_field": "Line integral of scalar fields",
+        "vector_field": "Line integral of vector fields",
+        "fundamental_theorem": "Fundamental theorem of line integrals",
+        "conservative_vector_field": "Conservative vector fields",
+        "green's_theorem": "Green's theorem"
+    } """
+
+    subtopics: dict[str, str] = {
+        "scalar_field": "Line integral of scalar fields",
+        "vector_field": "Line integral of vector fields"
+    }
+
+    def __init__(self, subtopic, dimension = 3, curve_is_parametric = True, curve_is_implicit = False, **kwargs):
+        super().__init__(self.topic[0], subtopic, **kwargs)
 
         self._dimension = dimension
 
-        if topic == "line_integral":
-            if subtopic not in ("vector_field", "scalar_field",
-                                "fundamental_theorem_of_line_integrals"):
-                msg = (f"{subtopic} is not a valid subtopic for the "
-                       "line_integral question topic")
-                raise ValueError(msg)
+        if subtopic not in self.subtopics:
+            msg = (f"{subtopic} is not a valid subtopic for the "
+                "line_integral question topic")
+            raise ValueError(msg)
 
-            field: VectorField | ScalarField = (
-                    VectorField("F", dimension) if subtopic == "vector_field"
-                    else ScalarField("phi", dimension)
-            )
-            linear_components = subtopic == "scalar_field"
-            curve: Curve = Curve(ambient_dim = dimension, linear_components = linear_components)
+        field: VectorField | ScalarField = (
+                VectorField("F", dimension) if subtopic == "vector_field"
+                else ScalarField("phi", dimension)
+        )
+        linear_components = subtopic == "scalar_field"
+        curve: Curve = Curve(ambient_dim = dimension, linear_components = linear_components)
 
-            answer: Expr = field.calculate_line_integral(curve.curve)
+        answer: Expr = field.calculate_line_integral(curve.curve)
+        answer_is_awkward = awkward_number(answer)
+
+        while answer_is_awkward:
+            field.regenerate()
+            curve.regenerate()
+
+            answer = field.calculate_line_integral(curve.curve)
             answer_is_awkward = awkward_number(answer)
-
-            while answer_is_awkward:
-                field.regenerate()
-                curve.regenerate()
-
-                answer = field.calculate_line_integral(curve.curve)
-                answer_is_awkward = awkward_number(answer)
 
         self._answer: str = self._generate_answer_latex(answer)
         self._question: str = self._generate_question_latex(field, curve)
@@ -69,17 +83,6 @@ class MultivariableCalculusQuestion(Question):
     @staticmethod
     def _generate_answer_latex(answer: str) -> str:
         return NoEscape(f"${latex(answer)}$")
-
-
-class LineIntegralQuestion(MultivariableCalculusQuestion):
-
-    subtopics: list[str] = [
-        "scalar_field", "vector_field", "fundamental_theorem_of_line_integrals",
-        "conservative_vector_field", "green's_theorem"
-    ]
-
-    def __init__(self, subtopic, dimension = 3, curve_is_parametric = True, curve_is_implicit = False, **kwargs):
-        super().__init__("line_integral", subtopic, dimension, curve_is_parametric, curve_is_implicit, **kwargs)
 
 class SurfaceIntegralQuestion(MultivariableCalculusQuestion):
     pass
--- core/question/question_registry.py
+++ core/question/question_registry.py
@@ -40,6 +40,7 @@ appropriate classes. The type hint "bool" refers to the optional parameter
 QUESTION_REGISTRY: dict[str, Type[Question]] = {}
 KEYWORD_REGISTRY: dict[str, Any] = {}
 TOPIC_REGISTRY: dict[str, list[str]] = {}
+TOPIC_DISPLAY_REGISTRY: dict[str, str] = {}
 
 def get_kwargs(func: Callable) -> dict[str, Any]:
     spec = getfullargspec(func)
@@ -50,21 +51,40 @@ def get_kwargs(func: Callable) -> dict[str, Any]:
     }
     return dict(list(reversed(kwargs.items())))
 
-def register_question_type(name: str) -> Callable[[Type[Question]], Type[Question]]:
+def register_type() -> Callable[[Type[Question]], Type[Question]]:
     def wrapper(cls: Type[Question]) -> Type[Question]:
-        QUESTION_REGISTRY[name] = cls
-        KEYWORD_REGISTRY[name] = get_kwargs(cls.__init__)
-        TOPIC_REGISTRY[name] = cls.subtopics
+        name_id = cls.name[0]
+        TOPIC_REGISTRY[name_id] = {}
+        TOPIC_DISPLAY_REGISTRY[name_id] = cls.name[1]
         return cls
     return wrapper
 
-def create_question(name: str, topic: str, subtopic: str, **kwargs: bool) -> Question:
-    if name == "question":
-        msg = f"{name} is not a valid question type"
+def register_question() -> Callable[[Type[Question]], Type[Question]]:
+    def wrapper(cls: Type[Question]) -> Type[Question]:
+        if not hasattr(cls, "topic"):
+            msg = f"{cls} doesn't have the topic attribute"
+            raise ValueError(msg)
+
+        topic_id = cls.topic[0]
+        QUESTION_REGISTRY[topic_id] = cls
+        KEYWORD_REGISTRY[topic_id] = get_kwargs(cls.__init__)
+
+        if hasattr(cls.__base__, "name"):
+            TOPIC_REGISTRY[cls.__base__.name[0]] = {topic_id: list(cls.subtopics.keys())}
+
+        TOPIC_DISPLAY_REGISTRY[topic_id] = cls.topic[1]
+        print(cls.subtopics)
+        TOPIC_DISPLAY_REGISTRY.update(cls.subtopics)
+        return cls
+    return wrapper
+
+def create_question(topic: str, subtopic: str, **kwargs: bool) -> Question:
+    if topic not in list(QUESTION_REGISTRY.keys()):
+        msg = f"{topic} is not a valid question type"
         raise ValueError(msg)
 
-    cls = QUESTION_REGISTRY.get(name)
+    cls = QUESTION_REGISTRY.get(topic)
     if cls is None:
-        msg = f"Unknown question type: {name}"
+        msg = f"Unknown question type: {topic}"
         raise ValueError(msg)
-    return cls(topic, subtopic, **kwargs)
+    return cls(subtopic, **kwargs)
