323613211f9a92f29003740031eda780eb1ec8f1
Andrew Phimister <157378252+Phim226@users.noreply.github.com>
2025-12-18 23:38:42 +0000

    bypass_guy adheres to new project structure

--- src/problem_sheet_generator/bypass_gui.py
+++ src/problem_sheet_generator/bypass_gui.py
@@ -1,9 +1,9 @@
 from logging import info
-from app.ui import SheetConfig
-from app.ui import QuestionConfig
-from core.sheet_generator import SheetGenerator
-from core.question import KEYWORD_REGISTRY, QUESTION_REGISTRY
-from utilities import configure_log
+from problem_sheet_generator.app.ui import SheetConfig
+from problem_sheet_generator.app.ui import QuestionConfig
+from problem_sheet_generator.core.sheet_generator import SheetGenerator
+from problem_sheet_generator.core.question import KEYWORD_REGISTRY, QUESTION_REGISTRY
+from problem_sheet_generator.utilities import configure_log
 
 # Use this code to bypass GUI.
 # TODO: Need to manually create QuestionConfig and pass appropriate arguments to generate() before this code can be used.
